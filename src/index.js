5d1e8c2788d9db6a85491f569d943048933ec812
knguyen1 <knguyen1@paypal.com>
2018-11-20 15:54:52 -0800

    Misc changes, incomplete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  state = {
+    count: 0,
+    windowObjectReference: null,
+    externalBody: null
+  };
+
+  // Increment the count
+  addCount = () => {
+    const newCount = this.state.count + 1;
+    this.setState({
+      count: newCount
+    });
+  };
+
+  openPopUp = () => {
+    if (
+      this.state.windowObjectReference === null ||
+      this.state.windowObjectReference.closed
+    ) {
+      // window.open(): https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+      const windowObjectReference = window.open(
+        // Empty to load a blank page
+        "",
+
+        // Empty so we can create a new window each time
+        "",
+
+        // List of window variables
+        "width=500,height=500,menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes"
+      );
+
+      this.setState({
+        windowObjectReference
+      });
+    } else {
+      this.windowObjectReference.focus();
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="count">{this.state.count}</div>
+        <button onClick={() => this.openPopUp()}>Open New Window</button>
+        {this.state.windowObjectReference &&
+          ReactDOM.createPortal(
+            <button onClick={() => this.addCount()}>+1</button>,
+            this.state.externalBody
+          )}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
